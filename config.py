1dd15853b474aaa16a4a868d84775e96e9fd327d
ULTR543 <pb006918@gmail.com>
2024-09-02 17:09:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7474678096:AAHZ6R0UsFNP0R1yYzBzbHa_BOYzqerKu-0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("ULTRA6T9")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5956579927")
 
 
 
